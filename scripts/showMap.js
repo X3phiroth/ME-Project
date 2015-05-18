b2169216dec8eedf864548aa2d929d73de0f0507
X3phiroth <ch.kozielski@icloud.com>
2015-05-18 09:36:09 +0200

    keep working...

---
+++ scripts/showMap.js
@@ -0,0 +1,29 @@
+function initialize(coords) {
+    var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    var myOptions = {
+        zoom: 16,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Hier bist du :)"
+    });
+}
+
+function start() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialize(position.coords);
+    }, function () {
+    });
+}
+google.maps.event.addDomListener(window, 'load', start);
+
+
+function displayMap() {
+    document.getElementById("map").style.display = "block";
+    
+}
